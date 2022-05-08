d3f4af43f3307868ea62a9b2875fc81e1951cf01
minaveyisoglu <minaveyisoglu1998@gmail.com>
2022-05-08 21:03:21 +0300

    code refactored

---
+++ NLayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction]//Endpoint olarak algılamasın
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            if (response.StatusCode == 204)
+            {
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode,
+                };              
+            }
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+            };
+        }
+    }
+}
